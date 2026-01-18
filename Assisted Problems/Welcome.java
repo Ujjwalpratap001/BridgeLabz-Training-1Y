10f76dedd9cfc3383210c5d937abd965559f90d2
Ujjwal Pratap <ujjwalpratapsingh624604@gmail.com>
2026-01-18 20:36:22 +0530

    first commit

--- Assisted Problems/Welcome.java
+++
@@ -1,6 +0,0 @@
-class Welcome{
-    // Write a program that prints "Welcome to Bridgelabz!" to the screen
-    public static void main(String[] args) {
-    System.out.println("Welcome to Bridgelabz");  
-}
-}
